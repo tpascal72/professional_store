e805b5ce80b8dd64e840be955d9ef896d4ea60f4
Thomas Pascal <tpascal72@academic.rrc.ca>
2014-04-14 22:54:48 -0500

    Added full search functionality on skills and category

--- app/controllers/Frontpage_controller.rb
+++ app/controllers/Frontpage_controller.rb
@@ -3,6 +3,6 @@ class FrontpageController < ApplicationController
   end
 
   def search_results #Displays search results
-  	@professionals = Professional.all
+  	@professionals = Professional.keyword_search(params[:search_keywords])
   end
 end
--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,9 +1,13 @@
 class Professional < ActiveRecord::Base
-	has_attached_file :image
-	has_many :rentals
-	belongs_to :category
+  has_attached_file :image
+  has_many :rentals
+  belongs_to :category
 
-	validates :fname, :lname, :cost_per_hour, :available, :skills, presence: true
-	validates :cost_per_hour, numericality: true, format: { with: /\A\d+\.?\d{0,2}\z/}
+  validates :fname, :lname, :cost_per_hour, :available, :skills, presence: true
+  validates :cost_per_hour, numericality: true, format: { with: /\A\d+\.?\d{0,2}\z/}
 
+  def self.keyword_search(keywords) #Displays search results
+  	keywords = "%" + keywords + "%"
+  	@professionals = Professional.where("skills LIKE ? OR category_id LIKE ?", keywords, keywords)
+  end
 end
