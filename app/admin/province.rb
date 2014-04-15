29139a4142616102cc9f9c622ba65f83a83c854d
Thomas Pascal <tpascal72@academic.rrc.ca>
2014-04-15 00:01:52 -0500

    Added provinces model for validation of up to 5 decimal places

--- app/admin/province.rb
+++ app/admin/province.rb
@@ -1,6 +1,5 @@
 ActiveAdmin.register Province do
 
-  
   permit_params :name, :pst, :gst, :hst
   
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -4,5 +4,5 @@ class Province < ActiveRecord::Base
 
 	validates :name, :pst, :gst, :hst, presence: true
 	validates :pst, :gst, :hst, numericality: true
-	validates :pst, :gst, :hst, format: { with: /\A\d+\.?\d{0,2}\z/}
+	validates :pst, :gst, :hst, format: { with: /\A\d+\.?\d{0,5}\z/}
 end
