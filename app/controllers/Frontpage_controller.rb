396cdfbd6c37698bfd7b62fcb24cd293694d875a
Thomas Pascal <tpascal72@academic.rrc.ca>
2014-04-16 17:32:02 -0500

    Improving design through SCSS

--- app/controllers/Frontpage_controller.rb
+++ app/controllers/Frontpage_controller.rb
@@ -13,18 +13,27 @@ class FrontpageController < ApplicationController
 
   def add_rental
   	# large validation ensures that all params must be present and 
-  	if !params[:professional_num].nil? && !params[:hours].nil? && !params[:days].nil? && !params[:start_days].nil? && !(session[:professional].include? params[:professional_num].to_i )
-  	  (session[:professional] ||= []) << params[:professional_num].to_i
-  	  (session[:hours] ||= []) << params[:hours]
-  	  (session[:days] ||= []) << params[:days]
-  	  (session[:start_days] ||= []) << params[:start_days]
-  	end
+  	if !session[:professional].nil?
+	  if !params[:professional_num].nil? && !params[:hours].nil? && !params[:days].nil? && !params[:start_days].nil? && !(session[:professional].include? params[:professional_num].to_i)
+	  	(session[:professional] ||= []) << params[:professional_num].to_i
+	  	(session[:hours] ||= []) << params[:hours]
+	  	(session[:days] ||= []) << params[:days]
+	  	(session[:start_days] ||= []) << params[:start_days]
+	  end
+	else
+	  if !params[:professional_num].nil? && !params[:hours].nil? && !params[:days].nil? && !params[:start_days].nil?
+	  	(session[:professional] ||= []) << params[:professional_num].to_i
+	  	(session[:hours] ||= []) << params[:hours]
+	  	(session[:days] ||= []) << params[:days]
+	  	(session[:start_days] ||= []) << params[:start_days]
+	  end
+	end
   end
 
   # "Create" a login, aka "log the user in"
   def corporate_log_in
 
-  	if params[:username] != nil && params[:password] != nil
+  	if !params[:username].nil? && !params[:password].nil?
 
       if user = Corporation.authenticate(params[:username], params[:password])
         # Save the user ID in the session so it can be used in
@@ -34,6 +43,13 @@ class FrontpageController < ApplicationController
       else
       	flash[:login_notice] = "Username/password incorrect."
       end
+    end
+    if !params[:logout].nil?
+      session[:current_user_id] = nil
+      session[:professional] = nil
+      session[:hours] = nil
+      session[:days] = nil
+      session[:start_days] = nil
     end
   end
 end
